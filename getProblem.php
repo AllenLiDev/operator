63deb832370824fb53520220f11d49fce59e317a
Stephen Cheng <aw3someone@gmail.com>
2016-05-09 20:34:55 -0700

    removed some useless code

--- getProblem.php
+++ getProblem.php
@@ -3,11 +3,6 @@ session_start();
 
 
 $difficulty = $_GET['difficulty'];
-
-
-// $difficulty = 5;
-
-//$hard = false;
 $hard = $_GET['hard'];
 if ($hard) {
     $upperCard = 13;
@@ -31,18 +26,9 @@ $result = $conn->query($sql);
 $randUpperBound = $result->num_rows;
 
 $rowNum = rand(1, $randUpperBound);
-// $sql = "SELECT * FROM `1-9Lv1` WHERE `rowNo` = 5";
-// $sql = "SELECT * FROM `1-9Lv" . $difficulty . "` WHERE `rowNo` = 5";
 $sql = "SELECT * FROM `1-" . $upperCard . "Lv" . $difficulty . "` WHERE `rowNo` = " . $rowNum;
 $problem = $conn->query($sql);
 $row = $problem->fetch_assoc();
-// echo $set;
-// echo $difficulty;
-// echo $rowNum;
-// echo $row["rowNo"];
-// echo $row["card1"];
-// echo $row["card2"];
-// echo $row["card3"];
-// echo $row["card4"];
+
 echo json_encode( array( "card1"=>$row["card1"], "card2"=>$row["card2"], "card3"=>$row["card3"], "card4"=>$row["card4"]));
 ?>
