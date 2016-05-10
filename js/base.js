f4c7a486ec0cba0a13734f85db9736fdd987649f
headhuntar <headhuntar@gmail.com>
2016-05-10 08:46:43 -0700

    random webpage stuff
    stephen was here

--- js/base.js
+++ js/base.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-	$('#drag').draggable();
-	$('#drop').droppable({
-		drop: handDropEvent
+	var plus = $('#operatorPlus').draggable({ revert: true });
+	var minus = $('#operatorMinus').draggable({ revert: true });
+	var multi = $('#operatorMulti').draggable({ revert: true });
+	var minus = $('#operatorDivide').draggable({ revert: true });
+	$('#drop1').droppable({
+		drop: dropped
 	});
 })
+
+function dropped(){
+	$(this).find("img").attr("src", "./img/operator1.png");
+}
