7c2c0690080ee42436cbcf7b66d079df06c73924
Stephen Cheng <Aw3someOne@gmail.com>
2016-05-13 13:46:10 -0700

    documenting php scripts

--- getProblem.php
+++ getProblem.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-
+// retrieve data from ajax request
 $difficulty = $_GET['difficulty'];
 $hard = $_GET['hard'];
 if ($hard) {
@@ -15,20 +15,24 @@ $username = "headhuntar";
 $password = "Group21rocks"; 
 $db = "headhunt_operator";
 
+// connects to database
 $conn = new mysqli($servername, $username, $password, $db);
 
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
 }
 
+// chooses table based on $upperCard value and $difficulty and gets the number of rows
 $sql = "SELECT * FROM `1-" . $upperCard . "Lv" . $difficulty . "`";
 $result = $conn->query($sql);
 $randUpperBound = $result->num_rows;
 
+// random row number from table
 $rowNum = rand(1, $randUpperBound);
 $sql = "SELECT * FROM `1-" . $upperCard . "Lv" . $difficulty . "` WHERE `rowNo` = " . $rowNum;
 $problem = $conn->query($sql);
 $row = $problem->fetch_assoc();
 
+// echoes json with card data
 echo json_encode( array( "card1"=>$row["card1"], "card2"=>$row["card2"], "card3"=>$row["card3"], "card4"=>$row["card4"]));
 ?>
--- timeAttackScore.php
+++ timeAttackScore.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+// retrieve data from ajax request
 $hard = $_GET['hard'];
 $name = $_GET['name'];
 $scores = $_GET['scores'];
@@ -10,24 +11,21 @@ if ($hard) {
 } else {
     $upperCard = 9;
 }
-// $upperCard = 9;
-// $name = 'ABC';
-// $scores = 123;
-// $date = 123456789012345;
 
 $servername = "localhost";
 $username = "headhuntar";
 $password = "Group21rocks"; 
 $db = "headhunt_operator";
 
+// connects to database
 $conn = new mysqli($servername, $username, $password, $db);
 
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
 }
 
-// $sql = "INSERT INTO `TimeAttackScores1-9`(`name`, `scores`, `dateKey`) VALUES ('abc', 123, 123)";
-// $sql = "INSERT INTO `TimeAttackScores1-" . $upperCard . "`(`name`, `scores`, `dateKey`) VALUES ('BOB', 123, 123456789012345)";
+// selects table based on $upperCard
 $sql = "INSERT INTO `TimeAttackScores1-" . $upperCard . "`(`name`, `scores`, `dateKey`) VALUES ('$name', $scores, $date)";
+// inserts score data into timeattackTable
 $conn->query($sql);
 $conn->close();
