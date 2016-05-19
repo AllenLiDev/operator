043e35efd9a8cb1f6649e4342a51304e2ddc5e4e
dw331297 <donnsjuan@gmail.com>
2016-05-19 10:03:18 -0700

    it loops

--- js/audio.js
+++ js/audio.js
@@ -6,9 +6,10 @@
 var music = [];
 var sfx = [];
 
-function sound(src) {
+function sound(src, loop) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
+    this.sound.loop = loop;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
@@ -30,21 +31,21 @@ function sound(src) {
 }
 
 /**/
-var menuMusic = new sound("./assets/audioAssets/menuMusic.mp3");
+var menuMusic = new sound("./assets/audioAssets/menuMusic.mp3", true);
 
-var gameMusic = new sound("./assets/audioAssets/gameMusic.mp3");
+var gameMusic = new sound("./assets/audioAssets/gameMusic.mp3", true);
 
 /**/
 
-var click = new sound("./assets/audioAssets/click.wav");
+var click = new sound("./assets/audioAssets/click.wav", false);
 
-var bass = new sound("./assets/audioAssets/bass.wav");
+var bass = new sound("./assets/audioAssets/bass.wav", false);
 
-var reward = new sound("./assets/audioAssets/reward.wav");
+var reward = new sound("./assets/audioAssets/reward.wav", false);
 
-var correct = new sound("./assets/audioAssets/correct.wav");
+var correct = new sound("./assets/audioAssets/correct.wav", false);
 
-var incorrect = new sound("./assets/audioAssets/incorrect.wav");
+var incorrect = new sound("./assets/audioAssets/incorrect.wav", false);
 
 /**/
 music.push(menuMusic, gameMusic);
