526cd0d8c149617f5d35602894ed31202056274d
dw331297 <donnsjuan@gmail.com>
2016-05-19 12:26:00 -0700

    audio.js comments and revision

--- js/audio.js
+++ js/audio.js
@@ -2,18 +2,18 @@
 /*                           AUDIO                                 */
 //****************************************************************//
 
-/**/
+/*Arrays for storing music and sound effect objects*/
 var music = [];
 var sfx = [];
 
+/*Constructor for sound objects - allows manual input of
+the file source, loop setting, and volume level*/
 function sound(src, loop, volume) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.loop = loop;
     this.sound.volume = volume;
     this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
     this.play = function(){
         if (gameArea.sound) {
             this.sound.play();
@@ -24,20 +24,19 @@ function sound(src, loop, volume) {
             this.sound.pause();
         }
     }
-    this.setCurrentTime = function(x){
+    this.setCurrentTime = function(int){
         if (gameArea.sound) {
-    	   this.sound.currentTime = x;
+    	   this.sound.currentTime = int;
         }
     }
 }
 
-/**/
+/*Background music*/
 var menuMusic = new sound("./assets/audioAssets/menuMusic.mp3", true, 0.5);
 
 var gameMusic = new sound("./assets/audioAssets/gameMusic.mp3", true, 0.5);
 
-/**/
-
+/*Sound Effects*/
 var click = new sound("./assets/audioAssets/click.wav", false, 1);
 
 var bass = new sound("./assets/audioAssets/bass.wav", false, 0.5);
@@ -48,7 +47,7 @@ var correct = new sound("./assets/audioAssets/correct.wav", false, 0.5);
 
 var incorrect = new sound("./assets/audioAssets/incorrect.wav", false, 0.5);
 
-/**/
+/*Populates audio arrays with created objects*/
 music.push(menuMusic, gameMusic);
 
 sfx.push(click, bass, reward, correct, incorrect);
